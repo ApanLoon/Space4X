a5467c802eac13b1bf1d66ad9b5f43e50bf48dcf
fre-ber <fre-ber@hotmail.com>
2018-12-08 11:48:44 +0100

    Toggle buttons for main view selection.

--- Space4X/Assets/Scripts/Controllers/MainViewController.cs
+++ Space4X/Assets/Scripts/Controllers/MainViewController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class MainViewController : MonoBehaviour
@@ -10,16 +9,39 @@ public class MainViewController : MonoBehaviour
 
     public GameObject SystemViewGO;
 
+    public event Action<MainView> OnMainViewChange;
+
+    public enum MainView
+    {
+        Galaxy,
+        System
+    }
+
+    public MainView CurrentMainView { get; protected set; }
+
+
     public void ShowGalaxyView()
     {
         SystemViewGO.SetActive(false);
         GalaxyViewGO.SetActive(true);
+        CurrentMainView = MainView.Galaxy;
+        RaiseOnMainViewChange();
     }
 
     public void ShowSystemView()
     {
         GalaxyViewGO.SetActive(false);
         SystemViewGO.SetActive(true);
+        CurrentMainView = MainView.System;
+        RaiseOnMainViewChange();
+    }
+
+    protected void RaiseOnMainViewChange()
+    {
+        if (OnMainViewChange != null)
+        {
+            OnMainViewChange(CurrentMainView);
+        }
     }
 
     private void OnEnable()
---
+++ Space4X/Assets/Scripts/Views/UI/MainViewPanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MainViewPanel : MonoBehaviour
+{
+    [SerializeField] protected ButtonToggle GalaxyViewToggle;
+    [SerializeField] protected ButtonToggle SystemViewToggle;
+
+    private void Start()
+    {
+        MainViewController.Instance.OnMainViewChange += OnMainViewChange;
+        OnMainViewChange(MainViewController.Instance.CurrentMainView);
+    }
+
+    private void OnMainViewChange(MainViewController.MainView view)
+    {
+        GalaxyViewToggle.SetToggle(view == MainViewController.MainView.Galaxy);
+        SystemViewToggle.SetToggle(view == MainViewController.MainView.System);
+    }
+}
